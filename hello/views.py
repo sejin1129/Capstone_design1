ecd9965fbfa24cecfa0d485b9560ac7e919cc93c
DeveloperYun <yhb7588@naver.com>
2022-10-14 15:19:05 +0900

    메인페이지 생성

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    response = HttpResponse()
+    response.write("hello everyone")
+    return response
